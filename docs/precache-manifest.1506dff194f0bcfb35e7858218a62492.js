137140062fc84e422adf877344b7a2e8e8df0012
DomLJ <dominik34@o2.pl>
2020-06-15 13:40:18 +0200

    Create build for demo

---
+++ docs/precache-manifest.1506dff194f0bcfb35e7858218a62492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a84290eb25f33452e0f01566e82a41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ad452aac6698de6f6d8",
+    "url": "/static/css/main.67430ffc.chunk.css"
+  },
+  {
+    "revision": "9eeaa3f70c83925cb9b7",
+    "url": "/static/js/2.11f86a92.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.11f86a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad452aac6698de6f6d8",
+    "url": "/static/js/main.fb8ff546.chunk.js"
+  },
+  {
+    "revision": "af7cd5ac159f8ab546bf",
+    "url": "/static/js/runtime-main.b53781ff.js"
+  }
+]);
